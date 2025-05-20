713a1b4677f3b8098420f7b83b2aa0991e88d6ed
MAtterholt <matterholt.dev@gmail.com>
2025-05-19 23:06:59 -0400

    working with fetch API

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+const events = ref(null)
+fetch('http://127.0.0.1:8000/sheep/event/')
+  .then((response) => response.json())
+  .then((data) => (events.value = data))
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div v-if="events">
+      <ul>
+        <li v-for="event in events">{{ event }} test</li>
+      </ul>
+    </div>
   </main>
 </template>
