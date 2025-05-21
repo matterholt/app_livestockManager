585038feafd05c861ae655b9c895ae92470a5bf7
MAtterholt <matterholt.dev@gmail.com>
2025-05-20 23:36:30 -0400

    working on entry form

---
+++ app/src/someTypes/livestockEvents.ts
@@ -0,0 +1,6 @@
+export interface LivestockEvents {
+  animal_tag: string
+  event_type: string
+  note: string[]
+  entry_date: string
+}
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
+import type { LivestockEvents } from '@/someTypes/livestockEvents'
+
+import LineListing from '@/components/core/LineListing.vue'
+import EventListForm from '@/components/core/eventListForm.vue'
+
 import { ref } from 'vue'
-const events = ref(null)
+const events = ref<LivestockEvents[] | null>(null)
 fetch('http://127.0.0.1:8000/sheep/event/')
   .then((response) => response.json())
   .then((data) => (events.value = data))
@@ -9,9 +14,16 @@ fetch('http://127.0.0.1:8000/sheep/event/')
 <template>
   <main>
     <div v-if="events">
-      <ul>
-        <li v-for="event in events">{{ event }} test</li>
-      </ul>
+      <LineListing :events="events" />
     </div>
+    <div v-else>
+      <p class="no_data">Reload</p>
+    </div>
+    <EventListForm />
   </main>
 </template>
+<style>
+.no_data {
+  color: red;
+}
+</style>
